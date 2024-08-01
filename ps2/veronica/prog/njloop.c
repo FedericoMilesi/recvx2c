c9d5be1f82c2da2213d531fca9c83e8feaa6e33a
BenoitRen <benoitren@benoitren.be>
2024-08-02 00:38:28 +0200

    Commit decompiled njloop.c.

--- ps2/veronica/prog/njloop.c
+++ ps2/veronica/prog/njloop.c
@@ -1,20 +1,19 @@
-
-
-
-
 void main();
 
-// 
-// Start address: 0x12b0a0
-void main()
-{
-	// Line 10, Address: 0x12b0a0, Func Offset: 0
-	// Line 11, Address: 0x12b0a8, Func Offset: 0x8
-	// Line 14, Address: 0x12b0b0, Func Offset: 0x10
-	// Line 15, Address: 0x12b0c0, Func Offset: 0x20
-	// Line 16, Address: 0x12b0c8, Func Offset: 0x28
-	// Line 18, Address: 0x12b0d0, Func Offset: 0x30
-	// Line 19, Address: 0x12b0d8, Func Offset: 0x38
-	// Func End, Address: 0x12b0e4, Func Offset: 0x44
-}
 
+
+
+
+
+
+
+void main() { // Line 10, Address: 0x12b0a0
+  njUserInit(); // Line 11, Address: 0x12b0a8
+
+loop_1:
+  if (njUserMain() >= 0) { // Line 14, Address: 0x12b0b0
+    njWaitVSync(); // Line 15, Address: 0x12b0c0
+    goto loop_1; // Line 16, Address: 0x12b0c8
+  }
+  njUserExit(); // Line 18, Address: 0x12b0d0
+} // Line 19, Address: 0x12b0d8
