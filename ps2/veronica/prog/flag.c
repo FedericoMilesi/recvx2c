b511c19844ee7f2759a1d6efb70d57de5a6211e6
BenoitRen <benoitren@benoitren.be>
2024-08-02 21:28:59 +0200

    Decompiled flag.c.

--- ps2/veronica/prog/flag.c
+++ ps2/veronica/prog/flag.c
@@ -1,35 +1,54 @@
-
-
-
-
 void bhStFlg(unsigned int* flg, unsigned int bit);
 void bhCrFlg(unsigned int* flg, unsigned int bit);
 int bhCkFlg(unsigned int* flg, unsigned int bit);
 
-// 
-// Start address: 0x280e40
-void bhStFlg(unsigned int* flg, unsigned int bit)
-{
-	// Line 37, Address: 0x280e40, Func Offset: 0
-	// Line 38, Address: 0x280e60, Func Offset: 0x20
-	// Func End, Address: 0x280e68, Func Offset: 0x28
-}
 
-// 
-// Start address: 0x280e70
-void bhCrFlg(unsigned int* flg, unsigned int bit)
-{
-	// Line 45, Address: 0x280e70, Func Offset: 0
-	// Line 46, Address: 0x280e94, Func Offset: 0x24
-	// Func End, Address: 0x280e9c, Func Offset: 0x2c
-}
 
-// 
-// Start address: 0x280ea0
-int bhCkFlg(unsigned int* flg, unsigned int bit)
-{
-	// Line 53, Address: 0x280ea0, Func Offset: 0
-	// Line 54, Address: 0x280ebc, Func Offset: 0x1c
-	// Func End, Address: 0x280ec4, Func Offset: 0x24
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void bhStFlg(unsigned int* flg, unsigned int bit) {
+    flg[bit / 32] |= 0x80000000 >> (bit & 31); // Line 37, Address: 0x280e40
+} // Line 38, Address: 0x280e60
+
+
+
+
+
+void bhCrFlg(unsigned int* flg, unsigned int bit) {
+  flg[bit / 32] &= ~(0x80000000 >> (bit & 31)); // Line 45, Address: 0x280e70
+} // Line 46, Address: 0x280e94
+
+
+
+
+
+int bhCkFlg(unsigned int* flg, unsigned int bit) {
+  return flg[bit / 32] & 0x80000000 >> (bit & 31); // Line 53, Address: 0x280ea0
+} // Line 54, Address: 0x280ebc
