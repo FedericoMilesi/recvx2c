0879dd6ab5506a4451536a3ff3cf8da0c8d55fd1
Federico <fmilesi_soft@hotmail.com>
2024-08-14 21:10:02 -0300

    Move matching % comment out of function signature on en06.c

--- ps2/veronica/prog/en06.c
+++ ps2/veronica/prog/en06.c
@@ -1749,7 +1749,8 @@ void bhEne06_Init(BH_PWORK* epw)
 	// Func End, Address: 0x1b9dc0, Func Offset: 0x450
 }
 
-void bhEne06_Brain(BH_PWORK* epw) /* Matching - 100% */ { // Line 597, Address: 0x1b9dc0
+/* Matching - 100% */
+void bhEne06_Brain(BH_PWORK* epw) { // Line 597, Address: 0x1b9dc0
     bhEne06_BrainType[epw->type](); 
 }
 
