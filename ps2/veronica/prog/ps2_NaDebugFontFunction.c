20611a67f9d03d0295680caf69b2a39413592e66
Federico <fmilesi_soft@hotmail.com>
2024-08-20 00:25:52 -0300

    Decompile ps2_NaDebugFontFunction.c

--- ps2/veronica/prog/ps2_NaDebugFontFunction.c
+++ ps2/veronica/prog/ps2_NaDebugFontFunction.c
@@ -1,50 +1,88 @@
-
-
-
-
 void njInitPrint();
 void njExitPrint();
 void njPrintSize();
 void njPrintColor();
 void njPrintC();
 
-// 
-// Start address: 0x2de110
-void njInitPrint()
-{
-	// Line 55, Address: 0x2de110, Func Offset: 0
-	// Func End, Address: 0x2de118, Func Offset: 0x8
-}
 
-// 
-// Start address: 0x2de120
-void njExitPrint()
-{
-	// Line 61, Address: 0x2de120, Func Offset: 0
-	// Func End, Address: 0x2de128, Func Offset: 0x8
-}
 
-// 
-// Start address: 0x2de130
-void njPrintSize()
-{
-	// Line 68, Address: 0x2de130, Func Offset: 0
-	// Func End, Address: 0x2de138, Func Offset: 0x8
-}
 
-// 
-// Start address: 0x2de140
-void njPrintColor()
-{
-	// Line 74, Address: 0x2de140, Func Offset: 0
-	// Func End, Address: 0x2de148, Func Offset: 0x8
-}
 
-// 
-// Start address: 0x2de150
-void njPrintC()
-{
-	// Line 88, Address: 0x2de150, Func Offset: 0
-	// Func End, Address: 0x2de158, Func Offset: 0x8
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* empty */
+void njInitPrint() { } // Line 55, Address: 0x2de110
+
+
+
+
+/* empty */
+void njExitPrint() { } // Line 61, Address: 0x2de120
+
+
+
+
+
+/* empty */
+void njPrintSize() { } // Line 68, Address: 0x2de130
+
+
+
+
+/* empty */
+void njPrintColor() { } // Line 74, Address: 0x2de140
+
+
+
+
+
+
+
+
+
+
+
+
+/* empty */
+void njPrintC() { } // Line 88, Address: 0x2de150
