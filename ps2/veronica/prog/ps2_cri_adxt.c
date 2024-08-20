80a9e1c58eb7dec28e73b1684e9e4fffc60d636c
Federico <fmilesi_soft@hotmail.com>
2024-08-19 21:30:48 -0300

    Decompile ps2_cri_adxt.c

--- ps2/veronica/prog/ps2_cri_adxt.c
+++ ps2/veronica/prog/ps2_cri_adxt.c
@@ -1,14 +1,18 @@
-
-
-
-
 void ADXT_Pause();
 
-// 
-// Start address: 0x2cb050
-void ADXT_Pause()
-{
-	// Line 18, Address: 0x2cb050, Func Offset: 0
-	// Func End, Address: 0x2cb058, Func Offset: 0x8
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* empty */
+void ADXT_Pause() { } // Line 18, Address: 0x2cb050
