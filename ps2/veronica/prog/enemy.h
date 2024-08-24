89fbe4aff7a519731e68c89cbd4994cde56c77be
Federico <fmilesi_soft@hotmail.com>
2024-08-24 05:30:07 -0300

    Decompile enemy.h

--- ps2/veronica/prog/enemy.h
+++ ps2/veronica/prog/enemy.h
@@ -1,14 +1,47 @@
-
-
-
-
 void bhEne28();
 
-// 
-// Start address: 0x1746f0
-void bhEne28()
-{
-	// Line 47, Address: 0x1746f0, Func Offset: 0
-	// Func End, Address: 0x1746f8, Func Offset: 0x8
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* empty */
+void bhEne28() { } // Line 47, Address: 0x1746f0
