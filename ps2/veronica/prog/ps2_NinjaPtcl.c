9f65337188b831c915cc57f5050721708f953098
Federico <fmilesi_soft@hotmail.com>
2024-08-24 02:54:47 -0300

    Decompile njPtclDrawSprite

--- ps2/veronica/prog/ps2_NinjaPtcl.c
+++ ps2/veronica/prog/ps2_NinjaPtcl.c
@@ -118,65 +118,59 @@ void njPtclSpriteStart(int texid, unsigned int col, int flag) { // Line 140, Add
 /* empty */
 void njPtclSpriteEnd() { } // Line 153, Address: 0x2d84e0
 
-// 
-// Start address: 0x2d84f0
-void njPtclDrawSprite(_anon0* p, int n, float w, float h)
-{
-	tagNJS_SCRVECTOR scr;
-	unsigned int col;
-	unsigned int i;
-	float bp[4];
-	float buff[4][256];
-	float invz;
-	// Line 164, Address: 0x2d84f0, Func Offset: 0
-	// Line 175, Address: 0x2d8520, Func Offset: 0x30
-	// Line 176, Address: 0x2d852c, Func Offset: 0x3c
-	// Line 178, Address: 0x2d8584, Func Offset: 0x94
-	// Line 179, Address: 0x2d8588, Func Offset: 0x98
-	// Line 180, Address: 0x2d858c, Func Offset: 0x9c
-	// Line 181, Address: 0x2d8590, Func Offset: 0xa0
-	// Line 182, Address: 0x2d8594, Func Offset: 0xa4
-	// Line 183, Address: 0x2d8598, Func Offset: 0xa8
-	// Line 184, Address: 0x2d859c, Func Offset: 0xac
-	// Line 187, Address: 0x2d85a0, Func Offset: 0xb0
-	// Line 189, Address: 0x2d85b0, Func Offset: 0xc0
-	// Line 207, Address: 0x2d85b8, Func Offset: 0xc8
-	// Line 188, Address: 0x2d85bc, Func Offset: 0xcc
-	// Line 189, Address: 0x2d85c0, Func Offset: 0xd0
-	// Line 190, Address: 0x2d85c4, Func Offset: 0xd4
-	// Line 189, Address: 0x2d85c8, Func Offset: 0xd8
-	// Line 190, Address: 0x2d85cc, Func Offset: 0xdc
-	// Line 191, Address: 0x2d85d0, Func Offset: 0xe0
-	// Line 192, Address: 0x2d85d4, Func Offset: 0xe4
-	// Line 189, Address: 0x2d85d8, Func Offset: 0xe8
-	// Line 206, Address: 0x2d85dc, Func Offset: 0xec
-	// Line 207, Address: 0x2d85e0, Func Offset: 0xf0
-	// Line 189, Address: 0x2d85e4, Func Offset: 0xf4
-	// Line 190, Address: 0x2d85ec, Func Offset: 0xfc
-	// Line 193, Address: 0x2d85f8, Func Offset: 0x108
-	// Line 190, Address: 0x2d85fc, Func Offset: 0x10c
-	// Line 191, Address: 0x2d8608, Func Offset: 0x118
-	// Line 192, Address: 0x2d8610, Func Offset: 0x120
-	// Line 193, Address: 0x2d8618, Func Offset: 0x128
-	// Line 194, Address: 0x2d8624, Func Offset: 0x134
-	// Line 193, Address: 0x2d8628, Func Offset: 0x138
-	// Line 194, Address: 0x2d8634, Func Offset: 0x144
-	// Line 195, Address: 0x2d864c, Func Offset: 0x15c
-	// Line 196, Address: 0x2d8654, Func Offset: 0x164
-	// Line 198, Address: 0x2d865c, Func Offset: 0x16c
-	// Line 199, Address: 0x2d8660, Func Offset: 0x170
-	// Line 200, Address: 0x2d8664, Func Offset: 0x174
-	// Line 201, Address: 0x2d8668, Func Offset: 0x178
-	// Line 203, Address: 0x2d866c, Func Offset: 0x17c
-	// Line 204, Address: 0x2d8670, Func Offset: 0x180
-	// Line 205, Address: 0x2d8674, Func Offset: 0x184
-	// Line 206, Address: 0x2d8678, Func Offset: 0x188
-	// Line 207, Address: 0x2d867c, Func Offset: 0x18c
-	// Line 209, Address: 0x2d8688, Func Offset: 0x198
-	// Line 210, Address: 0x2d869c, Func Offset: 0x1ac
-	// Line 212, Address: 0x2d86b0, Func Offset: 0x1c0
-	// Line 213, Address: 0x2d86b8, Func Offset: 0x1c8
-	// Line 216, Address: 0x2d86cc, Func Offset: 0x1dc
-	// Func End, Address: 0x2d8700, Func Offset: 0x210
+/* 100% match */
+void njPtclDrawSprite(NJS_POINT3* p, int n, float w, float h) { // Line 164, Address: 0x2d84f0
+    float(* bp)[4]; // modified from dwarf2cpp's output
+    unsigned int col;
+    unsigned int i;
+    float buff[256][4];
+    tagNJS_SCRVECTOR scr;
+    float invz; 
+    
+    
+    
+    
+    bp = (void*)&buff; // Line 175, Address: 0x2d8520
+    col = Ps2_ptcl_spr_col; // Line 176, Address: 0x2d852c
+    
+    for (i = 0; i < n; i++) { // Line 178, Address: 0x2d8584
+        *(unsigned int*)&bp[1][0] = (((col >> 16) & 0xFF) + 1) / 2; // Line 179, Address: 0x2d8588
+        *(unsigned int*)&bp[1][1] = (((col >> 8) & 0xFF) + 1) / 2; // Line 180, Address: 0x2d858c
+        *(unsigned int*)&bp[1][2] = ((col & 0xFF) + 1) / 2; // Line 181, Address: 0x2d8590
+        *(unsigned int*)&bp[1][3] = (((col >> 24) & 0xFF) + 1) / 2; // Line 182, Address: 0x2d8594
+        *(unsigned int*)&bp[4][0] = (((col >> 16) & 0xFF) + 1) / 2; // Line 183, Address: 0x2d8598
+        *(unsigned int*)&bp[4][1] = (((col >> 8) & 0xFF) + 1) / 2; // Line 184, Address: 0x2d859c
+
+        
+        *(unsigned int*)&bp[4][2] = ((col & 0xFF) + 1) / 2; // Line 187, Address: 0x2d85a0
+        *(unsigned int*)&bp[4][3] = (((col >> 24) & 0xFF) + 1) / 2; // Line 188, Address: 0x2d85bc
+        njRotTransPers(&p[i], &scr); // Line 189, Address: 0x2d85b0, 0x2d85c0, 0x2d85c8, 0x2d85d8, 0x2d85e4
+        invz = scr.iz; // Line 190, Address: 0x2d85c4, 0x2d85cc, 0x2d85ec, 0x2d85fc
+        bp[2][0] = scr.x - (invz * (w * _nj_screen_.dist)); // Line 191, Address: 0x2d85d0, 0x2d8608
+        bp[2][1] = scr.y - (invz * (h * _nj_screen_.dist)); // Line 192, Address: 0x2d85d4, 0x2d8610
+        bp[2][2] = scr.z; // Line 193, Address: 0x2d85f8, 0x2d8618, 0x2d8628
+        bp[2][3] = scr.fog; // Line 194, Address: 0x2d8624, 0x2d8634
+        bp[5][0] = scr.x + (invz * (w * _nj_screen_.dist)); // Line 195, Address: 0x2d864c
+        bp[5][1] = scr.y + (invz * (h * _nj_screen_.dist)); // Line 196, Address: 0x2d8654
+        
+        bp[5][2] = scr.z; // Line 198, Address: 0x2d865c
+        bp[5][3] = scr.fog; // Line 199, Address: 0x2d8660
+        bp[0][0] = 0; // Line 200, Address: 0x2d8664
+        bp[0][1] = 0; // Line 201, Address: 0x2d8668
+        
+        bp[0][2] = invz; // Line 203, Address: 0x2d866c
+        bp[0][3] = 0; // Line 204, Address: 0x2d8670
+        bp[3][0] = invz; // Line 205, Address: 0x2d8674
+        bp[3][1] = invz; // Line 206, Address: 0x2d8678
+        bp[3][2] = invz; // Line 207, Address: 0x2d867c
+        
+        bp[3][3] = 0; // Line 209, Address: 0x2d8688
+        bp += 6; // Line 210, Address: 0x2d869c
+    }
+    if (Ps2_ptcl_spr_flag == 1) { // Line 212, Address: 0x2d86b0
+        Ps2AddPrim3D(0x3B0000L << 32, &buff, n); // Line 213, Address: 0x2d86b8
+    } else {
+        Ps2AddPrim3D(0x1B0000L << 32, &buff, n); 
+    } // Line 216, Address: 0x2d86cc
 }
 
