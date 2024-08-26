57ee1b9e256addeb44cc30e90a53c0e918a40ed6
Federico <fmilesi_soft@hotmail.com>
2024-08-25 21:45:32 -0300

    Decompile njMemCopy4

--- ps2/veronica/prog/ps2_NaMem.c
+++ ps2/veronica/prog/ps2_NaMem.c
@@ -1,7 +1,3 @@
-
-
-
-
 void njMemCopy(void* vpDst, void* vpSrc, int lNum);
 void njMemCopy4(void* vpDst, void* vpSrc, int lNum);
 
@@ -16,14 +12,17 @@ void njMemCopy(void* vpDst, void* vpSrc, int lNum)
 	// Func End, Address: 0x2e3130, Func Offset: 0x30
 }
 
-// 
-// Start address: 0x2e3130
-void njMemCopy4(void* vpDst, void* vpSrc, int lNum)
-{
-	int* lpDst;
+/* 100% match */
+void njMemCopy4(void* vpDst, void* vpSrc, int lNum) { // Line 106, Address: 0x2e3130
+    int* lpDst;
 	int* lpSrc;
-	// Line 106, Address: 0x2e3130, Func Offset: 0
-	// Line 117, Address: 0x2e3158, Func Offset: 0x28
-	// Func End, Address: 0x2e3160, Func Offset: 0x30
-}
+    
+    for (lpSrc = vpSrc, lpDst = vpDst; lNum != 0; lpSrc++, lpDst++) { 
+        lNum--;
+        *lpDst = *lpSrc;
+    } 
+    
+    
+    
+} // Line 117, Address: 0x2e3158
 
