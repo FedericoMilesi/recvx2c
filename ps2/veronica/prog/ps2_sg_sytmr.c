5c5bfe213e78fbb3ff01f5e6422791314f1fff65
Federico <fmilesi_soft@hotmail.com>
2024-09-02 15:51:40 -0300

    Mark syTmrGetCount as empty

--- ps2/veronica/prog/ps2_sg_sytmr.c
+++ ps2/veronica/prog/ps2_sg_sytmr.c
@@ -1,14 +1,5 @@
-
-
-
-
 unsigned int syTmrGetCount();
 
-// 
-// Start address: 0x2de100
-unsigned int syTmrGetCount()
-{
-	// Line 5, Address: 0x2de100, Func Offset: 0
-	// Func End, Address: 0x2de108, Func Offset: 0x8
-}
 
+/* empty */
+unsigned int syTmrGetCount() { } // Line 5, Address: 0x2de100
