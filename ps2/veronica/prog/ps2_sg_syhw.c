725693b5e91a662a408eda445a5ecd104193478e
Federico <fmilesi_soft@hotmail.com>
2024-09-02 16:46:53 -0300

    Mark ps2_sg_syhw.c functions as empty

--- ps2/veronica/prog/ps2_sg_syhw.c
+++ ps2/veronica/prog/ps2_sg_syhw.c
@@ -1,32 +1,13 @@
-
-
-
-
 void syHwFinish();
 void syHwInit();
 void syHwInit2();
+/* empty */
+void syHwFinish() { } // Line 5, Address: 0x2ddf50
 
-// 
-// Start address: 0x2ddf50
-void syHwFinish()
-{
-	// Line 5, Address: 0x2ddf50, Func Offset: 0
-	// Func End, Address: 0x2ddf58, Func Offset: 0x8
-}
 
-// 
-// Start address: 0x2ddf60
-void syHwInit()
-{
-	// Line 9, Address: 0x2ddf60, Func Offset: 0
-	// Func End, Address: 0x2ddf68, Func Offset: 0x8
-}
+/* empty */
+void syHwInit() { } // Line 9, Address: 0x2ddf60
 
-// 
-// Start address: 0x2ddf70
-void syHwInit2()
-{
-	// Line 13, Address: 0x2ddf70, Func Offset: 0
-	// Func End, Address: 0x2ddf78, Func Offset: 0x8
-}
 
+/* empty */
+void syHwInit2() { } // Line 13, Address: 0x2ddf70
