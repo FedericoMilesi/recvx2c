baa179946d6f20f990fce829237e33b8b51458ae
Federico <fmilesi_soft@hotmail.com>
2024-09-02 19:21:34 -0300

    Decompiled syCfgSetSoundMode

--- ps2/veronica/prog/ps2_sg_sycfg.c
+++ ps2/veronica/prog/ps2_sg_sycfg.c
@@ -24,12 +24,7 @@ int syCfgGetSoundMode(int* pnMode)
 /* empty */
 int syCfgInit() { } // Line 18, Address: 0x2ddf30
 
-// 
-// Start address: 0x2ddf40
-int syCfgSetSoundMode(int nMode)
-{
-	// Line 22, Address: 0x2ddf40, Func Offset: 0
-	// Line 23, Address: 0x2ddf44, Func Offset: 0x4
-	// Func End, Address: 0x2ddf4c, Func Offset: 0xc
+/* 100% match */
+int syCfgSetSoundMode(int nMode) { // Line 22, Address: 0x2ddf40
+    SoundMode = nMode; // Line 23, Address: 0x2ddf44
 }
-
